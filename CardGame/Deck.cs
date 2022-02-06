f0c9171fc8d267c5dde3e40bee9dab57f93dde65
sheapa <patshea26@gmail.com>
2022-02-06 12:29:13 -0500

    Added deck generator, deck shuffler, Poker Deck

--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -8,9 +8,40 @@ namespace CardGame
 {
     public abstract class Deck
     {
+
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
         public void CreateDeck()
         {
+            fullDeck.Clear();
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard {Suit = (Enums.CardSuit) suit, Value = (Enums.CardValue) val});
 
+                ;
+                }
+            }
+
+        }
+
+        public virtual void ShuffleDeck()
+        {
+            //psuedo random
+            var rnd = new Random();
+            drawPile = fullDeck.OrderBy(x => rnd.Next()).ToList();
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard()
+        {
+            PlayingCard output = drawPile.Take(1).First();
+            drawPile.Remove(output);
+            return output;
         }
     }
 }
---
+++ CardGame/PokerDeck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public class PokerDeck : Deck
+    {
+        public PokerDeck()
+        {
+            CreateDeck();
+            ShuffleDeck();
+        }
+        public override List<PlayingCard> DealCard()
+        {
+            List<PlayingCard> output = new List<PlayingCard>();
+            for (int i = 0; i < 5; i++)
+            {
+                output.Add(RequestCard());
+            }
+            return output;
+        }
+    }
+}
