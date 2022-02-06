86759a9c0ab6d449b04c8319b87b3a1f4490ea47
sheapa <patshea26@gmail.com>
2022-02-06 11:32:43 -0500

    First Commit; Initial Set UP

---
+++ CardGame/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public class Enums
+    {
+        public enum CardSuit
+        {
+            Hearts,
+            Clubs,
+            Diamonds,
+            Spades
+        }
+
+        public enum CardValue
+        {
+            Ace,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+
+    }
+}
---
+++ CardGame/PlayingCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public class PlayingCard
+    {
+        public Enums.CardSuit Suit { get; set; }
+        public Enums.CardValue Value { get; set; }
+    }
+}
